f7237142a0573fc0d8df2aa2c3240f66377607e4
David Leen <dleen@uw.edu>
2012-10-24 19:00:30 -0700

    Started on database creation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,7 @@ import play.api.mvc._
 // jsoup html parser
 import org.jsoup._
 
+
 /* Allows us to treat Java objects as Scala objects.
    The main use is that jsoup.select.Elements is an instantiation
    of java.util.List[Element]. So this collection allows us
@@ -24,17 +25,29 @@ object Application extends Controller {
 
   val finalPage = pageNums(2).toInt
 
-  def returnPageN(pageN: Int, urlHead: String): List[String] = {
+  def returnPageN(pageN: Int, urlHead: String) = {
     val doc = Jsoup.connect(urlHead + pageN.toString).get()
     val searchResults = doc.getElementsByClass("search_result_row")
 
-    searchResults.map(_.select("a[href]").text).toList
+    println(searchResults.first)
+
+    searchResults.map(gameValues(_)).toList
   }
 
+  def gameValues(gameHtml: org.jsoup.nodes.Element): List[String] = {
+    List(gameHtml.getElementsByClass("search_price").text,
+      gameHtml.getElementsByClass("search_released").text,
+      gameHtml.select("h4").text,
+      gameHtml.getElementsByClass("search_metascore").text,
+      gameHtml.select("a").attr("href"))
+      // add more here
+  }
+  
   val textResults = returnPageN(1, steamStoreHead)
-
+  println(textResults)
+  
   def index = Action {
-    Ok(views.html.index(textResults))
+    Ok(views.html.index(List("hello")))
   }
 
 }
