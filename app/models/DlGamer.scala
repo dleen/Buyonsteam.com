ff9c2d0b5e3ca8da9f8c1e404cbbb616bbd793d2
Light <leenf@tcd.ie>
2012-10-31 14:34:05 +0000

    Added DlGamer Scraper

---
+++ app/models/DlGamer.scala
@@ -0,0 +1,61 @@
+package models
+
+import anorm._
+import java.util.Date
+import org.jsoup._
+import org.jsoup.nodes._
+import scala.collection.JavaConversions._
+
+case class DlGamerScraper(pageN: Int = 1) extends Scraper with SafeMoney { 
+
+  def getAll: List[GwithP] = scrapePage(pageN, allVals)
+  def getGames: List[Game] = scrapePage(pageN, gameVals)
+  def getPrices: List[Price] = scrapePage(pageN, priceVals)
+
+  def scrapePage[A](pageN: Int, f: (Element) => A): List[A] = { 
+    val doc = Jsoup.connect(GamersGateDets.storeHead + pageN.toString)
+      .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+      .get()
+    val searchResults = doc.getElementsByClass("box").toList
+
+    searchResults.map(f(_))
+  }
+
+  def gameVals(html: Element): Game = {
+    val name = html.getElementsByClass("title").text
+    val gameUrl = html.getElementsByClass("title").attr("href")
+    val imgUrl = html.select("img").attr("src")
+
+    Game(NotAssigned, name, GamersGateDets.name, gameUrl, imgUrl)
+  }
+
+  def priceVals(html: Element): Price = {
+    val name = html.getElementsByClass("title").text
+    val priceS = $anitizer(html.getElementsByClass("price").text)
+    val onSale = !html.getElementsByClass("old-price").isEmpty
+
+    Price(NotAssigned, priceS, onSale, new Date(), 0)
+  }
+
+}
+
+object DlGamerDets extends StoreDetails { 
+
+  val name = "DlGamer"
+
+  val storeHead =
+    "http://www.dlgamer.us/download-pc_games-c-27.html?page="
+
+  def getFinalPage(storeTail: String = "1"): Int = { 
+    val doc = Jsoup.connect(storeHead + storeTail).get()
+    val paginator = doc.getElementsByClass("numberpage").select("a").toList
+    val last = paginator.last.text
+    if(last == storeTail) storeTail.toInt
+    else finalPage(last)
+  }
+  
+// TODO - Find final page automatically
+  val finalPage = getFinalPage("1")
+  
+}
+
