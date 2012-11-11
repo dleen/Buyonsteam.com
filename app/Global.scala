97b077daff9f976656e5c6c6e3d1909deba68d40
David Leen <dleen@uw.edu>
2012-11-10 17:47:45 -0800

    minor

--- app/Global.scala
+++ app/Global.scala
@@ -10,12 +10,12 @@ object Global extends GlobalSettings {
 
   val SS = ActorSystem("ScraperScheduler")
   val Runner = SS.actorOf(Props(new Runner), name = "Runner")
-  val Runner1 = SS.actorOf(Props(new Runner), name = "Runner1")
+  //val Runner1 = SS.actorOf(Props(new Runner), name = "Runner1")
 
   override def onStart(app: Application) {
 
     SS.scheduler.schedule(30.seconds, 1.hours, Runner, Gogo)
-    SS.scheduler.schedule(10.seconds, 30.seconds, Runner, Gogo1)
+    //SS.scheduler.schedule(10.seconds, 30.seconds, Runner, Gogo1)
 
   }
 
